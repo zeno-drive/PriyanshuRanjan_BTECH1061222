c011fc69b202c20290930bd5eae24ddbe1797a76
zeno-drive <zenoindrive@gmail.com>
2026-01-13 17:46:07 +0530

    implement database-backed task creation and retrieval

--- Backend/app/routes/tasks.py
+++ Backend/app/routes/tasks.py
@@ -1,12 +1,24 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
 from app.schemas.task import TaskCreate, TaskResponse
+from app.models.task import Task
+from app.dependencies import get_db
 
 router=APIRouter()
 
 @router.post("/", response_model=TaskResponse)
-def create_task(task: TaskCreate):
-    return{
-        "id":1,
-        "title": task.title,
-        "status": task.status
-    }
+def create_task(task: TaskCreate, db:Session= Depends(get_db)):
+    new_task=Task(
+        title=task.title,
+        status=task.status
+    )
+    db.add(new_task)
+    db.commit()
+    db.refresh(new_task)
+    return new_task
+    
+@router.get("/", response_model=list[TaskResponse])
+def get_tasks(db: Session = Depends(get_db)):
+    tasks = db.query(Task).all()
+    return tasks
